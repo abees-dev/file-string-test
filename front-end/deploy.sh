cf2fcfb0129dffc078cf77dd94303c86610853df
abees <abeesdevjs@gmail.com>
2023-02-22 11:26:01 +0700

    fix github action

--- front-end/deploy.sh
+++ front-end/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 tar --exclude="node_modules" --exclude="dist" --exclude=".env" --exclude=".github" --exclude="yarn.lock" --exclude=".idea" --exclude="deploy"   -zcvf SERVICE.tgz *
 cp SERVICE.tgz ./docker && rm -rf SERVICE.tgz
-cd ./docker && chmod +x deploy.sh
